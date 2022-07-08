3ad8c081de90932347f26310a672cb04a38418ec
mangoo <cjo9701@gmail.com>
2022-07-09 02:41:09 +0900

    0709

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
   var mixer = mixitup('.item-list');
+
+  let navigation = document.querySelector('.navigation');
+  let mobile_nav = document.querySelector('.mobile_nav');
+  let toggle = document.querySelector('.toggle');
+  
+  toggle.onclick = function(){
+    mobile_nav.classList.toggle('active')
+    navigation.classList.toggle('active')
+  }
 });
